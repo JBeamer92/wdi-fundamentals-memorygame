ad1d7a9ba44a3714043e6e9d1873cef026d798a0
Jared Beamer <jared@Jareds-MacBook-Pro.local>
2017-05-22 07:33:50 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,25 +3,21 @@ var cards = ['queen', 'queen', 'king', 'king'];
 //create empty array for selected cards
 var cardsInPlay = [];
 
-//create variable for first selected card
-var cardOne = cards[0];
-//add selected card to cardsInPlay array
-cardsInPlay.push(cardOne);
-
-//for testing purposes
-console.log('User flipped queen');
-
-var cardTwo = cardsInPlay[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped king');
-
-////checks that 2 cards have been selected
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert('You found a match!');
-    } else {
-        alert('Sorry, try again!');
+var flipCard = function(cardId) {
+    // logs selected card
+    console.log("User flipped " + cards[cardId]);
+    // adds selected card to cardsInPlay array
+    cardsInPlay.push(cards[cardId]);
+    // checks that 2 cards have been selected
+    // checks for match, displays appropriate result
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert('You found a match!');
+        } else {
+            alert('Sorry, try again!');
+        }
     }
-}
+} // end function flipCard
+
+flipCard(0);
+flipCard(2);
